2c8086a674e932b7544bfeb22bee613c07776d86
Prometheus <33342399+Prometheus-03@users.noreply.github.com>
2023-08-12 12:59:36 +0800

    Fixed formatting issues

--- js/pages/List.js
+++ js/pages/List.js
@@ -103,24 +103,20 @@ export default {
                             </li>
                         </ol>
                     </template>
-                    <h3>Submission Requirements</h3>
+                    <h3> Submission Requirements </h3>
                     <p> When submitting a record, please ensure that you have the following:</p>
-                    <ul>
-                        <li> A complete playthrough of the level from 0-100 with no cuts (if you make cuts in your submitted video, include raw footage that doesn't have them) </li>
-                        <li> A decent amount of previous attempts (A single death at 1% is not sufficient, try to get somewhat far into the level. Everplay records are exempt from this.) </li>
-                        <li> End stats (The whole box must appear for at least one frame) </li>
-                        <li> Cheat Indicator (If you are using a mod menu that supports one, like Megahack v7) </li>
-                        <li> Fps/tps indicator (For mod menus that support one) </li>
-                        <li> In-game source audio/Clicks (Either is fine, however both are strongly recommended. If you don't have either in your submission video, attach raw footage that does) </li>
-                    </ul>
+                    <p> - A complete playthrough of the level from 0-100 with no cuts (if you make cuts in your submitted video, include raw footage that doesn't have them) </p>
+                    <p> - A decent amount of previous attempts (A single death at 1% is not sufficient, try to get somewhat far into the level. Everplay records are exempt from this.) </p>
+                    <p> - End stats (The whole box must appear for at least one frame) </p>
+                    <p> - Cheat Indicator (If you are using a mod menu that supports one, like Megahack v7) </p>
+                    <p> - Fps/tps indicator (For mod menus that support one) </p>
+                    <p> - In-game source audio/Clicks (Either is fine, however both are strongly recommended. If you don't have either in your submission video, attach raw footage that does) </p>
                     <p> Refer to <a href="https://docs.google.com/spreadsheets/d/1evE4nXATxRAQWu2Ajs54E6cVUqHBoSid8I7JauJnOzg/edit#gid=0">this sheet</a> for a complete list of allowed mods.</p>
                     <p> Please also check for the following:</p>
-                    <ul>
-                        <li>Make sure you beat the level displayed on the site (for reference, check the level ID to ensure you're playing the correct level</li>
-                        <li>Do not use secret routes or bug routes</li>
-                        <li>Do not use easy modes, only a record of the unmodified level qualifies</li>
-                        <li>Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level</li>
-                    </ul>
+                    <p> - Make sure you beat the level displayed on the site (for reference, check the level ID to ensure you're playing the correct level</p>
+                    <p> - Do not use secret routes or bug routes</p>
+                    <p> - Do not use easy modes, only a record of the unmodified level qualifies</p>
+                    <p> - Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level</p>
                 </div>
             </div>
         </main>
