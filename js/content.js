69330c4ba58ae79b77775f077ab71c1fdce3c47c
Prometheus <33342399+Prometheus-03@users.noreply.github.com>
2023-12-19 23:40:27 +0800

    New update

--- js/content.js
+++ js/content.js
@@ -7,16 +7,22 @@ const dir = '/data';
 
 export async function fetchList() {
     const listResult = await fetch(`${dir}/_list.json`);
+    const packResult = await fetch(`${dir}/_packlist.json`);
     try {
         const list = await listResult.json();
+        const packsList = await packResult.json();
         return await Promise.all(
             list.map(async (path, rank) => {
                 const levelResult = await fetch(`${dir}/${path}.json`);
                 try {
                     const level = await levelResult.json();
+                    let packs = packsList.filter((x) =>
+                        x.levels.includes(path)
+                    );
                     return [
                         {
                             ...level,
+                            packs,
                             path,
                             records: level.records.sort(
                                 (a, b) => b.percent - a.percent,
@@ -48,7 +54,7 @@ export async function fetchEditors() {
 
 export async function fetchLeaderboard() {
     const list = await fetchList();
-
+    const packResult = await (await fetch(`${dir}/_packlist.json`)).json();
     const scoreMap = {};
     const errs = [];
     list.forEach(([level, err], rank) => {
@@ -65,6 +71,7 @@ export async function fetchLeaderboard() {
             verified: [],
             completed: [],
             progressed: [],
+            packs: [],
         };
         const { verified } = scoreMap[verifier];
         verified.push({
@@ -72,6 +79,7 @@ export async function fetchLeaderboard() {
             level: level.name,
             score: score(rank + 1, 100, level.percentToQualify),
             link: level.verification,
+            path: level.path
         });
 
         // Records
@@ -83,6 +91,8 @@ export async function fetchLeaderboard() {
                 verified: [],
                 completed: [],
                 progressed: [],
+                packs: [],
+                path: level.path
             };
             const { completed, progressed } = scoreMap[user];
             if (record.percent === 100) {
@@ -91,6 +101,7 @@ export async function fetchLeaderboard() {
                     level: level.name,
                     score: score(rank + 1, 100, level.percentToQualify),
                     link: record.link,
+                    path: level.path
                 });
                 return;
             }
@@ -101,9 +112,22 @@ export async function fetchLeaderboard() {
                 percent: record.percent,
                 score: score(rank + 1, record.percent, level.percentToQualify),
                 link: record.link,
+                path: level.path
             });
         });
     });
+
+    for (let user of Object.entries(scoreMap)) {
+        let levels = [...user[1]["verified"], ...user[1]["completed"]].map(
+            (x) => x["path"]
+        );
+
+        for (let pack of packResult) {
+            if (pack.levels.every((e1) => levels.includes(e1))) {
+                user[1]["packs"].push(pack);
+            }
+        }
+    }
 
     // Wrap in extra Object containing the user and total score
     const res = Object.entries(scoreMap).map(([user, scores]) => {
@@ -121,4 +145,43 @@ export async function fetchLeaderboard() {
 
     // Sort by total score
     return [res.sort((a, b) => b.total - a.total), errs];
+}
+
+export async function fetchPacks() {
+    try {
+        const packResult = await fetch(`${dir}/_packlist.json`);
+        const packsList = await packResult.json();
+        return packsList;
+    } catch {
+        return null;
+    }
+}
+
+export async function fetchPackLevels(packname) {
+    const packResult = await fetch(`${dir}/_packlist.json`);
+    const packsList = await packResult.json();
+    const selectedPack = await packsList.find((pack) => pack.name == packname);
+    try {
+        return await Promise.all(
+            selectedPack.levels.map(async (path, rank) => {
+                const levelResult = await fetch(`${dir}/${path}.json`);
+                try {
+                    const level = await levelResult.json();
+                    return [
+                        {
+                            level,
+                            path,
+                        },
+                        null,
+                    ];
+                } catch {
+                    console.error(`Failed to load level #${rank + 1} ${path} (${packname}).`);
+                    return [null, path];
+                }
+            })
+        );
+    } catch (e) {
+        console.error(`Failed to load packs.`, e);
+        return null;
+    }
 }
